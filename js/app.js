3149a0b538964f117138b49db030772bc16ff5c6
margb13 <marg.b13@hotmail.com>
2018-02-20 03:48:32 -0500

    Agregando evento click para el cambio de vista

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 'use strict';
+$(document).ready(function() {
+  let openFareEstimate = $('.btn-estimate');
+  openFareEstimate.click(function() {
+    window.location.href = 'views/estimate.html';
+  });
+});
